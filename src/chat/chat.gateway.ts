41e7e4a07000df69e574335dd63171095a5ad329
ewjee <ewjee@puzzleone.com>
2024-06-24 12:07:02 +0000

    :sparkles: Add Redis Adapter

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,9 +1,52 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from 'src/utils/logger';
+import { ChatService } from './chat.service';
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { createAdapter } from "@socket.io/redis-adapter";
+import { Redis } from "ioredis";
 
-@WebSocketGateway()
-export class ChatGateway {
+@WebSocketGateway({ path: process.env.SOCKET_END_POINT, cors: { origin: '*' }, namespace: '/chat' })
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  private pubClient: Redis;
+  private subClient: Redis;
+  constructor(
+    private readonly chatService: ChatService,
+    private readonly logger: Logger,
+  ) {
+    this.server = new Server({adapter : createAdapter(this.pubClient , this.subClient)});
+    this.pubClient =  new Redis(process.env.REDIS_URL ?? "redis://username:authpassword@127.0.0.1:6380/4"),
+    this.subClient = this.pubClient.duplicate()
+  }
+  @WebSocketServer()
+  server: Server;
+
+  //Function that gets called on connection
+  async handleConnection(client: Socket) {
+    this.logger.log( "info" , `Client connected: ${client.id}`);
+  }
+
+  //Function that gets called on connection lost
+  async handleDisconnect(client: Socket) {
+    this.logger.log( "info" , `Client disconnected: ${client.id}`);
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(client: Socket, room: string): void {
+    client.join(room);
+    this.logger.log("info" , `Client ${client.id} joined room ${room}`);
+    this.server.to(room).emit('message', `Client ${client.id} joined room ${room}`);
+  }
+
+  @SubscribeMessage('leaveRoom')
+  handleLeaveRoom(client: Socket, room: string): void {
+    client.leave(room);
+    this.logger.log("info" , `Client ${client.id} left room ${room}`);
+    this.server.to(room).emit('message', `Client ${client.id} left room ${room}`);
+  }
+
   @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+  handleMessage(client: Socket, data: { room: string, message: string }): void {
+    this.logger.log("info" , `Message from ${client.id} to room ${data.room}: ${data.message}`);
+    this.server.to(data.room).emit('message', { clientId: client.id, message: data.message });
   }
 }
--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { ChatRepository } from './chat.repository';
+import { ChatDocument } from './chat.schema';
+import { CreateChatDao } from './dao/createChatDao';
+import { FindChatDao } from './dao/findChatDao';
+import { UpdateChatDao } from './dao/updateChatDao';
+import { ClientSession } from 'mongoose';
 
 @Injectable()
 export class ChatService {
     constructor (
         private readonly chatRepository: ChatRepository,
     ){}
+    async createChat(createChatDao : CreateChatDao , session : ClientSession): Promise<ChatDocument | null>{
+        try {
+            const newChat = await this.chatRepository.createChat(createChatDao , session);
+            return newChat;
+        } catch (error){
+            return null;
+        }
+    }
+
+    async findChats(findChatDao : FindChatDao , session : ClientSession , projectionOptions: any = null, findOptions: any = null , limitValue: number = 0): Promise<ChatDocument[] | null>{
+        try {
+            const newChats = await this.chatRepository.findChats(findChatDao , session , projectionOptions , findOptions , 0);
+            return newChats;
+        } catch (error){
+            return null;
+        }
+    }
+
+    async findChat(findChatDao : FindChatDao , session : ClientSession , projectionOptions: any = null, findOptions: any = null): Promise<ChatDocument | null>{
+        try {
+            const newChat = await this.chatRepository.findChat(findChatDao , session , projectionOptions , findOptions);
+            return newChat;
+        } catch (error){
+            return null;
+        }
+    }
+
+    async updateChat(findChatDao : FindChatDao , updateChatDao : UpdateChatDao , session : ClientSession): Promise<ChatDocument | null>{
+        try {
+            const newChat = await this.chatRepository.updateChat(findChatDao , updateChatDao , session);
+            return newChat;
+        } catch (error){
+            return null;
+        }
+    }
+
+    async deleteChat(findChatDao : FindChatDao , session : ClientSession): Promise<boolean>{
+        try {
+            const deleted = await this.chatRepository.deleteChat(findChatDao ,  session);
+            return deleted;
+        } catch (error){
+            return false;
+        }
+    }
 }
